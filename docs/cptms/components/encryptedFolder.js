f29310c4a7550539c127905fd9ac4ae31938eccb
David Tudury <david.tudury@gmail.com>
2020-08-17 22:03:53 -0700

    fields

--- docs/cptms/components/encryptedFolder.js
+++ docs/cptms/components/encryptedFolder.js
@@ -1,7 +1,14 @@
 import { h } from '../horseless.js'
+import input from './input.js'
+import button from './button.js'
+import octicons from '../octicons.js'
 
 export default function (attributes) {
   const model = attributes.model
+  const handleSubmit = el => e => {
+    e.preventDefault()
+    console.log(e.target)
+  }
   /*
   attributes.style = `
     font-size: inherit;
@@ -18,11 +25,26 @@ export default function (attributes) {
       </div>
     `
   } else {
-    setTimeout(() => {
-      model.module = './encryptedFolder.js'
-    }, 1000)
     return h`
-      fields to init folder
+      <form onsubmit=${handleSubmit}>
+        <div style="display: flex; align-items: center; font-weight: bold;">
+          ${octicons('dot-16', { style: 'padding: 8px;' })} 
+          Initialize Repository:
+        </div>
+        <label style="display:flex; align-items: center;">
+          ${octicons('key-16', { style: 'padding: 8px;' })} 
+          Passphrase:
+          <${input} name="passphrase" style="flex-grow: 1;" required/>
+        </label>
+        <label style="display:flex; align-items: center;">
+          ${octicons('gear-16', { style: 'padding: 8px;' })} 
+          Iterations:
+          <${input} name="iterations" type="number" defaultValue="20000" style="flex-grow: 1;"/>
+        </label>
+        <div style="display: flex; align-items: center; justify-content: flex-end;">
+          <${button} type="submit">${octicons('shield-lock-16', { style: 'padding-right: 4px;' })} Set Encryption</button>
+        </div>
+      </form>
     `
   }
 }
