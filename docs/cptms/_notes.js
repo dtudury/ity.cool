1288d15c246af76bfa8d6499e29ce860c227949d
David Tudury <david.tudury@gmail.com>
2020-08-26 22:52:43 -0700

    forgot to save changes to notes...

--- docs/cptms/_notes.js
+++ docs/cptms/_notes.js
@@ -1,33 +1,5 @@
-export const asdf = {
-  keyStuffSize: 33,
-  keystuff: [],
-  firstEntrySize: 36, // encrypted 4 bytes
-  entries: [
-    { // encrypted json
-      attributes: {},
-      childrenSizes: [0, 34, 48]
-    }
-  ]
-}
 
-export const qwerty = {
-  // attribute part
-  a: ['attributeSize', 'dataSize'],
-  b: 10,
-  // data part
-  data: [
-    'encrypted: {c: 14'
-  ]
-}
-
-export const fubar = {
-  keystuff: {},
-  data: [
-    // first one is root
-  ]
-}
-
-export const bogo = [
+export const serialize = [
   /* magic number? */
   /* same prefix as below */'unencrypted data: nonce, iteration count',
   [
@@ -43,7 +15,7 @@ export const bogo = [
 diff'able; folder should have a method for catching up. does small structure solve this? hash tree
 */
 
-export const explicit = {
+export const state = {
   address: '0',
   selected: {
     address: '0.1',
@@ -51,31 +23,24 @@ export const explicit = {
       address: '0.1.0'
     }
   },
-  expanded: {
-    0.1: {
-      '0.1.0': {}
-    },
-    0.2: {}
-  }
+  expanded: [0.1, '0.1.0', 0.2]
 }
 
-export const dataByAddress = {
+export const props = {
   0: {
     name: 'root',
-    address: '0',
     module: './seeker.js',
-    objectStoreWrapper: {}
+    objectStoreWrapper: {},
+    children: []
   },
   0.1: {
     name: 'folder',
-    address: '0.1',
     module: './seeker.js',
     objectStoreWrapper: {},
     children: []
   },
   '0.1.0': {
     name: 'document',
-    address: '0.1.0',
     module: './whatever.js',
     objectStoreWrapper: {},
     children: []
