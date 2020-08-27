f1efce781d10de6696ab2a8fe3fb90ac914c70f4
David Tudury <david.tudury@gmail.com>
2020-08-27 16:39:28 -0700

    deepset function to clobber less

--- docs/cptms/cptms.js
+++ docs/cptms/cptms.js
@@ -2,6 +2,19 @@ import { render, h, proxy, watchFunction } from './horseless.js'
 import { ObjectStoreWrapper } from './db.js'
 import seeker from './components/seeker.js'
 
+function deepSet (src, dest, key) {
+  if (dest[key] && typeof src === typeof dest[key] && typeof src === 'object') {
+    Object.getOwnPropertyNames(dest[key]).forEach(name => {
+      if (!Object.prototype.hasOwnProperty.call(src, name)) {
+        delete dest[key][name]
+      }
+    })
+    Object.getOwnPropertyNames(src).forEach(name => deepSet(src[name], dest[key], name))
+  } else {
+    dest[key] = src
+  }
+}
+
 function removePrefix (prefix, str) {
   if (str.startsWith(prefix)) {
     return str.substr(prefix.length)
@@ -12,7 +25,7 @@ function removePrefix (prefix, str) {
 const model = window.model = proxy({})
 function hashToModel () {
   const hash = window.location.hash.substr(1) || '0'
-  console.log('starting with hash', hash)
+  console.log('starting with hash', window.location.hash)
   let prefix = ''
   const state = hash.split(':').map(substr => {
     const [addressSuffix, expandedSuffices] = substr.split(';')
@@ -25,7 +38,8 @@ function hashToModel () {
       return { address }
     }
   })
-  model.state = state
+  // model.state = state
+  deepSet(state, model, 'state')
   console.log('ending with model', JSON.stringify(model, null, '  '))
 }
 function modelToHash () {
@@ -39,11 +53,13 @@ function modelToHash () {
     }
     return substr
   }).join(':')}`
-  window.history.pushState(null, null, hash)
+  if (window.location.hash !== hash) {
+    window.history.pushState(null, null, hash)
+  }
   console.log('ending with hash', hash)
 }
 window.onhashchange = hashToModel
-window.onpopstate = hashToModel
+// window.onpopstate = hashToModel
 hashToModel()
 watchFunction(modelToHash)
 /*
