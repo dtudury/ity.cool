97841c3865086a8bf30572d38ff85f1b447b605c
David Tudury <david.tudury@gmail.com>
2020-10-10 15:49:18 -0700

    hookup some more ui

--- docs/cptms/components/panel.js
+++ docs/cptms/components/panel.js
@@ -3,18 +3,52 @@ import { getParentAddress } from '../address.js'
 import octicons from '../octicons.js'
 
 function fileListItemView ({ child, state, select, selected }, children, description) {
-  const chevronHandlers = el => ({
+  const isExpanded = () => state.expanded.some(({ address }) => address === child.address)
+  const chevronDown = octicons('chevron-down-16', el => ({
     onclick: e => {
       e.stopPropagation()
-      console.log(el, e)
-      console.log(JSON.parse(JSON.stringify(state)))
+      console.log('close')
+      for (let i = state.expanded.length - 1; i >= 0; i--) {
+        if (state.expanded[i].address === child.address) {
+          state.expanded.splice(i, 1)
+        }
+      }
     }
-  })
+  }))
+  const chevronRight = octicons('chevron-right-16', el => ({
+    onclick: e => {
+      e.stopPropagation()
+      console.log('open')
+      state.expanded.push({
+        address: child.address,
+        expanded: []
+      })
+    }
+  }))
   return h`<div onclick=${el => select} style="border: 1px solid red;">
-    ${showIfElse(() => state.expanded.indexOf(child.address) !== -1, octicons('chevron-down-16', chevronHandlers), octicons('chevron-right-16', chevronHandlers))}
+    ${showIfElse(isExpanded, chevronDown, chevronRight)}
     ${() => JSON.stringify(child.name)}
     ${showIfElse(() => state.selected && state.selected.address === child.address, octicons('north-star-16'))}
   </div>`
+}
+
+function fileListView ({ data, state, indent }) {
+  const select = child => el => e => {
+    state.selected = {
+      address: child.address,
+      expanded: []
+    }
+  }
+  return h`
+    ${mapEntries(() => data.children, child => h`
+      <${fileListItemView}
+        select=${select(child)}
+        child=${child}
+        state=${state}
+        indent=${indent}
+      />
+    `)}
+  `
 }
 
 export default function panel ({ state, model }, children, description) {
@@ -32,25 +66,24 @@ export default function panel ({ state, model }, children, description) {
         })
       }
     }
-    const select = child => el => e => {
-      state.selected = {
-        address: child.address,
-        expanded: []
-      }
-    }
     if (file.data) {
       model.focus = state.address
       const data = file.data
       return h`
         <section id=${state.address} style="flex: 1 0 20em; overflow-y: scroll;">
           ${name}
-          ${mapEntries(() => data.children, child => h`
-            <${fileListItemView}
-              select=${select(child)}
-              child=${child}
-              state=${state}
-            />
-          `)}
+          <${fileListView}
+            data=${data}
+            state=${state}
+            indent=${0}
+          />
+        </section>
+        <${panel} state=${state.selected} model=${model}/>
+      `
+    } else {
+      return h`
+        <section id=${state.address} style="flex: 1 0 20em; overflow-y: scroll;">
+          loading...
         </section>
         <${panel} state=${state.selected} model=${model}/>
       `
