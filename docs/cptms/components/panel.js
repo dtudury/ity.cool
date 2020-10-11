804ad5b0481bbefa2fc30055b278c424832eddb8
David Tudury <david.tudury@gmail.com>
2020-10-11 00:24:04 -0700

    reuse

--- docs/cptms/components/panel.js
+++ docs/cptms/components/panel.js
@@ -1,5 +1,4 @@
 import { h, mapEntries, showIfElse } from '../horseless.js'
-import { getParentAddress } from '../address.js'
 import octicons from '../octicons.js'
 
 function fileListItemView ({ child, state, select, selected }, children, description) {
@@ -23,70 +22,63 @@ function fileListItemView ({ child, state, select, selected }, children, descrip
         address: child.address,
         expanded: []
       })
+      console.log(JSON.parse(JSON.stringify(state)))
     }
   }))
-  return h`<div onclick=${el => select} style="border: 1px solid red;">
-    ${showIfElse(isExpanded, chevronDown, chevronRight)}
-    ${() => JSON.stringify(child.name)}
-    ${showIfElse(() => state.selected && state.selected.address === child.address, octicons('north-star-16'))}
-  </div>`
-}
-
-function fileListView ({ data, state, indent }) {
-  const select = child => el => e => {
-    state.selected = {
-      address: child.address,
-      expanded: []
-    }
-  }
   return h`
-    ${mapEntries(() => data.children, child => h`
-      <${fileListItemView}
-        select=${select(child)}
-        child=${child}
-        state=${state}
-        indent=${indent}
-      />
+    <div onclick=${el => select} style="border: 1px solid red;">
+      ${showIfElse(isExpanded, chevronDown, chevronRight)}
+      ${() => JSON.stringify(child.name)}
+      ${showIfElse(() => state.selected && state.selected.address === child.address, octicons('north-star-16'))}
+    </div>
+    ${showIfElse(isExpanded, h`
+      open
     `)}
   `
 }
 
+const fileListViews = new Map()
+function fileListView ({ state, model, indent }, children, description) {
+  const file = model.get(state.address)
+  const data = file.data
+  if (data) {
+    model.focus = state.address
+  }
+  const select = child => el => e => {
+    state.selected = state.selected || {}
+    state.selected.address = child.address
+    state.selected.expanded = []
+  }
+  if (!fileListViews.has(description)) {
+    fileListViews.set(description, showIfElse(() => model.get(state.address).data, h`
+      ${mapEntries(() => model.get(state.address).data.children, child => h`
+        <${fileListItemView}
+          select=${select(child)}
+          child=${child}
+          state=${state}
+          indent=${indent}
+        />
+      `)}
+    `, 'loading...'))
+  }
+  return fileListViews.get(description)
+}
+
+const panels = new Map()
 export default function panel ({ state, model }, children, description) {
   if (state && state.address) {
-    const file = model.get(state.address)
-    const parentAddress = getParentAddress(state.address)
-    let name = 'root'
-    if (parentAddress) {
-      const parentFile = model.get(parentAddress)
-      if (parentFile && parentFile.data) {
-        (parentFile.data.children || []).forEach(child => {
-          if (child.address === state.address) {
-            name = child.name
-          }
-        })
-      }
-    }
-    if (file.data) {
-      model.focus = state.address
-      const data = file.data
-      return h`
-        <section id=${state.address} style="flex: 1 0 20em; overflow-y: scroll;">
-          ${name}
+    if (!panels.has(description)) {
+      panels.set(description, h`
+        <section id=${() => state.address} style="flex: 1 0 20em; overflow-y: scroll;">
           <${fileListView}
-            data=${data}
-            state=${state}
+            state=${() => state}
+            model=${model}
             indent=${0}
           />
         </section>
-        <${panel} state=${state.selected} model=${model}/>
-      `
-    } else {
-      return h`
-        <section id=${state.address} style="flex: 1 0 20em; overflow-y: scroll;">
-          loading...
-        </section>
-        <${panel} state=${state.selected} model=${model}/>
-      `
+        <${panel} state=${() => state.selected} model=${model}/>
+      `)
     }
+    return panels.get(description)
   }
 }
