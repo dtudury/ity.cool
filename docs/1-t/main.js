d72ca712adb1a71cc6c0521188c39693ecdcee9e
David Tudury <david.tudury@gmail.com>
2020-12-31 02:37:12 -0800

    feels good

--- docs/1-t/main.js
+++ docs/1-t/main.js
@@ -6,8 +6,9 @@ const model = window.model = proxy(localStorage.getItem('model') ? JSON.parse(lo
   c: { x: 200, y: 200 },
   d: { x: 100, y: 200 },
   v: { x: 150, y: 150 },
-  showingStringArt: true,
-  strings: 9
+  showingSpine: true,
+  showingStringArt: false,
+  steps: 32
 })
 watchFunction(() => { localStorage.setItem('model', JSON.stringify(model)) })
 
@@ -21,45 +22,96 @@ const draggableCircle = (p, attributes = { r: 5 }) => {
   }
   return circle(p, attributes, ongrab)
 }
-const cubic = (a, b, c, d, t, r = 1 - t) => a * t * t * t + 3 * b * t * t * r + 3 * c * t * r * r + d * r * r * r
-const cubicP = (a, b, c, d, t) => ({ x: cubic(a.x, b.x, c.x, d.x, t), y: cubic(a.y, b.y, c.y, d.y, t) })
-const linear = (a, b, t, r = 1 - t) => a * t + r * b
-const linearP = (a, b, t) => ({ x: linear(a.x, b.x, t), y: linear(a.y, b.y, t) })
+const coeffecients = [[], [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]]
+const bezier = (values, t) => values.reduce((a, b, i) => {
+  const weight = coeffecients[values.length][i] * Math.pow(t, i) * Math.pow(1 - t, values.length - i - 1)
+  return a + b * weight
+}, 0)
+const bezierP = (points, t) => points.reduce((a, b, i) => {
+  const weight = coeffecients[points.length][i] * Math.pow(t, i) * Math.pow(1 - t, points.length - i - 1)
+  return { x: a.x + b.x * weight, y: a.y + b.y * weight }
+}, { x: 0, y: 0 })
 
+const distance = (a, b) => Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2))
+const add = (a, b) => ({ x: a.x + b.x, y: a.y + b.y })
+const sub = (a, b) => add(a, { x: -b.x, y: -b.y })
+const scale = (a, v) => ({ x: a.x * v, y: a.y * v })
+const dot = (a, b) => a.x * b.x + a.y * b.y
+const rot = (a, angle) => {
+  const s = Math.sin(angle)
+  const c = Math.cos(angle)
+  return {
+    x: a.x * c - a.y * s,
+    y: a.x * s + a.y * c
+  }
+}
+const atan = a => Math.atan2(a.y, a.x)
 const p2xy = p => `${p.x} ${p.y}`
 const toSteps = (steps, f) => steps > 1 ? Array(steps).fill().map((_, i) => f(i / (steps - 1))) : null
 const line = (a, b, attributes) => h`<path d="M${() => p2xy(a)}L${() => p2xy(b)}" ${attributes}/>`
 const curve = (a, b, c, d, attributes) => h`<path d="M${() => p2xy(a)}C${() => p2xy(b)} ${() => p2xy(c)} ${() => p2xy(d)}" ${attributes}/>`
-const markers = (a, b, c, d, attributes) => el => toSteps(model.strings, t => circle(cubicP(a, b, c, d, t), attributes))
-const sub = (a, b, c, d, t, attributes) => {
-  const ab = linearP(a, b, t)
-  const bc = linearP(b, c, t)
-  const cd = linearP(c, d, t)
-  const abbc = linearP(ab, bc, t)
-  const bccd = linearP(bc, cd, t)
-  return [line(ab, bc, attributes), line(bc, cd, attributes), line(abbc, bccd, attributes)]
+const stretchedCurve = (a, b, c, d, v, attributes) => el => {
+  const av = Math.pow(distance(a, v), Math.SQRT2)
+  const dv = Math.pow(distance(d, v), Math.SQRT2)
+  const t = av / (av + dv)
+  const ab = bezierP([a, b], t)
+  const bc = bezierP([b, c], t)
+  const cd = bezierP([c, d], t)
+  const abbc = bezierP([ab, bc], t)
+  const bccd = bezierP([bc, cd], t)
+  const abbcbccd = bezierP([abbc, bccd], t)
+  let dspin = atan(sub(v, d)) - atan(sub(abbcbccd, d))
+  let aspin = atan(sub(v, a)) - atan(sub(abbcbccd, a))
+  if (Math.abs(aspin - dspin) > Math.PI) {
+    if (aspin < dspin) aspin += Math.PI * 2
+    else dspin += Math.PI * 2
+  }
+  const spin = bezier([aspin, dspin], t)
+  const abv = add(v, rot(sub(abbc, abbcbccd), spin))
+  const vcd = add(v, rot(sub(bccd, abbcbccd), spin))
+  return [
+    curve(a, ab, abv, v, attributes),
+    curve(v, vcd, cd, d, attributes)
+  ]
 }
-const subs = (a, b, c, d, attributes) => el => toSteps(model.strings, t => sub(a, b, c, d, t, attributes))
-const tLines = (a, b, c, d) => el => {
-  const angle = Math.atan2(d.y - a.y, d.x - a.x)
-  const unitAD = { x: Math.cos(angle), y: Math.sin(angle) }
-  console.log(unitAD)
-  return toSteps(model.strings, t => circle(linearP(a, d, t), { r: 2 }))
+const markers = (a, b, c, d, attributes) => el => toSteps(model.steps, t => circle(bezierP([a, b, c, d], t), attributes))
+const stringart = (a, b, c, d, t, attributes) => {
+  const ab = bezierP([a, b], t)
+  const bc = bezierP([b, c], t)
+  const cd = bezierP([c, d], t)
+  const abbc = bezierP([ab, bc], t)
+  const bccd = bezierP([bc, cd], t)
+  return [
+    line(ab, bc, attributes),
+    line(bc, cd, attributes),
+    line(abbc, bccd, attributes)
+  ]
+}
+const stringarts = (a, b, c, d, attributes) => el => toSteps(model.steps, t => stringart(a, b, c, d, t, attributes))
+const projection = (a, b, points) => {
+  const d = distance(a, b)
+  const unit = scale(sub(b, a), 1 / d)
+  return points.map(point => add(a, scale(unit, dot(unit, sub(point, a)))))
+}
+const tLines = (a, b, c, d, attributes) => el => {
+  const [bad, cad] = projection(a, d, [b, c])
+  return toSteps(model.steps, t => line(bezierP([a, bad, cad, d], t), bezierP([a, b, c, d], t), attributes))
 }
 
-const stringsChange = el => e => { model.strings = +el.value }
+const stringsChange = el => e => { model.steps = +el.value }
 const showStringsChange = el => e => { model.showingStringArt = el.checked }
+const showSpineChange = el => e => { model.showingSpine = el.checked }
 
 render(document.body, h`
   <svg width="100vw" height="100vh" style="display: block;" xmlns="http://www.w3.org/2000/svg">
-    ${line(model.b, model.a, { stroke: 'blue', 'stroke-dasharray': '0 10 10 0' })}
-    ${line(model.c, model.d, { stroke: 'blue', 'stroke-dasharray': '0 10 10 0' })}
-    ${curve(model.a, model.b, model.c, model.d, { fill: 'none', stroke: 'red' })}
+    ${line(model.b, model.a, { stroke: 'red', 'stroke-dasharray': '0 10 10 0' })}
+    ${line(model.c, model.d, { stroke: 'red', 'stroke-dasharray': '0 10 10 0' })}
+    ${stretchedCurve(model.a, model.b, model.c, model.d, model.v, { fill: 'none', stroke: 'green' })}
     ${showIfElse(() => model.showingStringArt, [
       markers(model.a, model.b, model.c, model.d, { r: 2, fill: 'none', stroke: '#ff000066' }),
-      subs(model.a, model.b, model.c, model.d, { r: 2, fill: 'none', stroke: '#ff000033' })
+      stringarts(model.a, model.b, model.c, model.d, { r: 2, fill: 'none', stroke: '#ff000033' })
     ])}
-    ${tLines(model.a, model.b, model.c, model.d)}
+    ${showIfElse(() => model.showingSpine, [tLines(model.a, model.b, model.c, model.d, { stroke: '#00ffff88' })])}
     ${draggableCircle(model.a, { r: 7, fill: 'black', stroke: 'none', 'pointer-events': 'all' })}
     ${draggableCircle(model.b, { r: 8, fill: 'none', stroke: 'black', 'pointer-events': 'all' })}
     ${draggableCircle(model.c, { r: 8, fill: 'none', stroke: 'black', 'pointer-events': 'all' })}
@@ -84,9 +136,13 @@ render(document.body, h`
       <input type="checkbox" oninput=${showStringsChange} name="showStrings" ${() => model.showingStringArt ? { checked: true } : null}>
     </div>
     <div>
-      <label for="strings">Strings</label>
-      <input type="range" oninput=${stringsChange} name="strings" value="${() => model.strings}" min="3" max="100">
-      ${() => model.strings}
+      <label for="steps">Steps</label>
+      <input type="range" oninput=${stringsChange} name="steps" value="${() => model.steps}" min="3" max="100">
+      ${() => model.steps}
+    </div>
+    <div>
+      <label for="showSpine">Show Spine</label>
+      <input type="checkbox" oninput=${showSpineChange} name="showSpine" ${() => model.showingSpine ? { checked: true } : null}>
     </div>
   </details>
 `)
