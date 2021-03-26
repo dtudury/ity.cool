8e63ffbb447d2d952e2096e7c5dd6cdd71b07a4d
David Tudury <dt@reduct.video>
2021-03-26 06:24:37 -0700

    play with tags

--- docs/moded/main.js
+++ docs/moded/main.js
@@ -1,23 +1,33 @@
-import { watchFunction, render, h, copyGently } from './horseless.0.5.3.min.esm.js' // '/unpkg/horseless/horseless.js'
-import { model, saveModel } from './model.js'
-import { A_ELEMENT } from './modules/aElement.js'
-import { B_ELEMENT } from './modules/bElement.js'
+import {
+  watchFunction,
+  render,
+  h,
+  copyGently
+} from "./horseless.0.5.3.min.esm.js"; // '/unpkg/horseless/horseless.js'
+import { model, saveModel } from "./model.js";
+import { A_ELEMENT } from "./modules/aElement.js";
+import { B_ELEMENT } from "./modules/bElement.js";
 
-const a = {a:1, b: 2, c: 3}
-const x = {x:100, y: 200, z: 300}
+const a = { a: 1, b: 2, c: 3 };
+const x = { x: 100, y: 200, z: 300 };
 
-const q = copyGently(a, x)
-console.log(q, q === a)
-console.log(q, q === x)
+const q = copyGently(a, x);
+console.log(q, q === a);
+console.log(q, q === x);
 
-function updateDimensions () {
-  model.dimensions = { width: window.innerWidth, height: window.innerHeight }
+function updateDimensions() {
+  model.dimensions = { width: window.innerWidth, height: window.innerHeight };
 }
-updateDimensions()
-window.addEventListener('resize', updateDimensions, false)
-watchFunction(saveModel)
+updateDimensions();
+window.addEventListener("resize", updateDimensions, false);
+watchFunction(saveModel);
 
-render(document.body, h`
+render(
+  document.body,
+  h`
   <${A_ELEMENT}/>
-  <${B_ELEMENT}/>
-`)
+  <${B_ELEMENT} o=${{ a: 1, b: 2 }}>
+    w <b>x</b> h: ${() => model.dimensions.width} <b>x</b> ${() => model.dimensions.height}
+  </closing-tag-name-ignored?!>
+`
+);
--- docs/moded/modules/bElement.js
+++ docs/moded/modules/bElement.js
@@ -8,30 +8,30 @@ class BElement extends window.HTMLElement {
     render(
       this.shadowRoot,
       h`
-      <style>
-        :host {
-          display: block;
-          width: 100px;
-          height: 100px;
-          background: green;
-          border: 1px solid green;
-        }
-        div {
-          background: blue;
-          border-radius: 20px;
-        }
-      </style>
-      <div ${this.getDivAttributes} ${(...args) => {
-        console.log("no this", args);
-      }}>asdf</div>
-      ${() => !!model.div}
-    `
+        <style>
+          :host {
+            display: block;
+            width: 200px;
+            height: 100px;
+            background: green;
+            border: 1px solid green;
+          }
+          div {
+            background: yellow;
+            border-radius: 20px;
+          }
+        </style>
+        <div ${this.getDivAttributes} ${(...args) => {
+          console.log("no this", args, this);
+        }}>slot here => <slot/></div>
+        ${() => !!model.div}
+      `
     );
     console.log(this.shadowRoot.querySelector("div"));
     model.div = this.shadowRoot.querySelector("div");
   }
   getDivAttributes(...args) {
-    console.log("this", args);
+    console.log("this", args, this);
     return [];
   }
   connectedCallback() {}
@@ -41,6 +41,7 @@ const bElementName = "b-element";
 window.customElements.define(bElementName, BElement);
 const tieUps = new Map();
 export const B_ELEMENT = (attributes, children, description) => {
-  if (!tieUps.has(description)) tieUps.set(description, h`<${bElementName}/>`);
+  console.log(attributes, children)
+  if (!tieUps.has(description)) tieUps.set(description, h`<${bElementName}>${children}</>`);
   return tieUps.get(description);
 };
