27aaf6fb40ff7e2236f27dd667bbbd2f63556f04
David Tudury <dt@reduct.video>
2021-04-01 14:07:35 -0700

    wip

--- docs/gentle/main.js
+++ docs/gentle/main.js
@@ -1,16 +1,70 @@
-import { render, h } from "./horseless.0.5.3.min.esm.js"; // '/unpkg/horseless/horseless.js'
+import { render, h, mapEntries } from "./horseless.0.5.3.min.esm.js"; // '/unpkg/horseless/horseless.js'
 import { model } from "./model.js";
+const scale = 10;
 
 const lines = el => {
-  if (model.nodesByTime) {
+  if (model.edgesByTime) {
+    const output = [];
+    const tWidth = window.innerWidth / scale;
+    const innerHeight = window.innerHeight;
+    for (const t in model.edgesByTime) {
+      if (t < model.offsetT + tWidth && t > model.offsetT - model.maxDt) {
+        const offsetT = t - model.offsetT;
+        const groups = model.edgesByTime[t];
+        let i = 0;
+        const stackHeight = Object.keys(groups).length;
+        for (const groupString in groups) {
+          const { transitions, word, dt } = JSON.parse(groupString);
+          const x = offsetT * scale;
+          const y = innerHeight * i / stackHeight
+          const width = dt * scale;
+          const height = 20;
+          output.push(h`
+            <rect x="${x}" y="${y}" width="${width}" height="${height}" fill="none" stroke="white"/>
+            <text 
+              x="${x + width / 2}" 
+              y="${y + height / 2}" 
+              dominant-baseline="middle"
+              text-anchor="middle"
+            >${word}</text>
+          `);
+          ++i;
+        }
+      }
+    }
+    return output;
   }
 };
+
+document.onscroll = () => {
+  model.offsetT = window.scrollX / scale;
+};
+
+const styleElement = document.createElement("style");
+document.head.appendChild(styleElement);
+render(
+  styleElement,
+  h`
+    body {
+      margin: 0;
+      width: ${() => model.maxT * scale}px;
+    }
+    svg {
+      position: fixed;
+      height: 100vh;
+      width: 100vw;
+      display: block;
+      background: oldlace;
+    }
+  `
+);
 
 render(
   document.body,
   h`
-    <svg width="100vw" height="100vh" style="display: block; background: oldlace;" xmlns="http://www.w3.org/2000/svg">
+    <svg xmlns="http://www.w3.org/2000/svg">
       ${lines}
     </svg>
+    without some text it won't scroll
   `
 );
--- docs/gentle/model.js
+++ docs/gentle/model.js
@@ -1,85 +1,120 @@
 import { proxy } from "./horseless.0.5.3.min.esm.js"; // '/unpkg/horseless/horseless.js'
 
+export const model = (window.model = proxy({ offsetT: 0 }));
+
+const phonemes = new Set();
 const edgeToString = edge => {
-  return JSON.stringify(edge.transitions) + edge.word || "";
-  return (
-    JSON.stringify(
-      (edge.transitions || []).map(({ phone }) => phone.split("_")[0])
-    ) + (edge.word || "")
-  );
-  /*
-  const edgeCopy = JSON.parse(JSON.stringify(edge));
-  delete edgeCopy.from_state;
-  return JSON.stringify(edgeCopy.transitions);
-  */
+  edge.transitions.forEach(({ phone }) => phonemes.add(phone.split("_")[0]));
+  return JSON.stringify({
+    transitions: edge.transitions,
+    word: edge.word,
+    dt: edge.dt
+  });
 };
 
-export const model = (window.model = proxy());
 (async () => {
   const lat = await (await fetch("lat.json")).json();
+  let maxT = 0;
+  let maxDt = 0;
   const edges = [];
+  const edgesByTime = {};
+  let latticeIndex = 0;
   lat.forEach(({ lattice }) => {
+    const duration = edge =>
+      (edge.transitions || []).reduce((a, { length }) => a + length, 0);
+    const edgesByFrom = {};
+    // const edgesByTo = {};
     for (const i in lattice) {
       for (const edge of lattice[i]) {
-        edges.push(edgeToString(edge));
+        const edgeIndex = edges.length;
+        edge.latticeIndex = latticeIndex;
+        edge.dt = duration(edge);
+        maxDt = Math.max(maxDt, edge.dt);
+        edges.push(edge);
+        const toState = edge.to_state;
+        const fromState = edge.from_state;
+        if (toState != null && fromState != null) {
+          edgesByFrom[fromState] = edgesByFrom[fromState] || [];
+          edgesByFrom[fromState].push({ toState, edgeIndex });
+          // edgesByTo[toState] = edgesByTo[toState] || [];
+          // edgesByTo[toState].push({ fromState, edgeIndex });
+        }
       }
     }
-  });
-  console.log(new Set(edges));
-  /*
-  edges.sort();
-  let dup = 0;
-  let prev;
-  edges.forEach(edge => {
-    if (edge !== prev) console.log(edge);
-    if (edge === prev) ++dup;
-    prev = edge;
-  });
-  */
-  // console.log(edges.length, dup);
-  /*
-  model.nodesByTime = [];
-  console.log(lat);
-  lat.forEach(({ lattice }) => {
-    // console.log(lattice)
-    const graph = [];
-    for (const i in lattice) {
-      const edges = lattice[i];
-      for (const edge of edges) {
-        const { from_state, to_state } = edge;
-        if (from_state == null || to_state == null) break;
-        if (!graph[from_state]) {
-          graph[from_state] = { i: from_state, in: [], out: [] };
+    const calculateTimes = (state, t) => {
+      (edgesByFrom[state] || []).forEach(({ toState, edgeIndex }) => {
+        edgesByTime[t] = edgesByTime[t] || {};
+        const edgeString = edgeToString(edges[edgeIndex]);
+        const group = (edgesByTime[t][edgeString] =
+          edgesByTime[t][edgeString] || []);
+        const dt = edges[edgeIndex].dt;
+        maxT = Math.max(maxT, t + dt);
+        if (!group.includes(edgeIndex)) {
+          group.push(edgeIndex);
+          calculateTimes(toState, t + dt);
         }
-        if (!graph[to_state]) {
-          graph[to_state] = { i: to_state, in: [], out: [] };
-        }
-        const dt = edge.transitions.reduce((a, { length }) => a + length, 0);
-        graph[from_state].out.push({ i: to_state, edge, dt });
-        graph[to_state].in.push({ i: from_state, edge, dt });
-      }
-    }
-    calculateTimes(0, graph);
-    // console.log(graph);
-    const nodesByTime = {};
-    graph.forEach(node => {
-      const t = node.t;
-      nodesByTime[t] = nodesByTime[t] || [];
-      nodesByTime[t].push(node);
-    });
-    model.nodesByTime.push(nodesByTime);
-    console.log(nodesByTime);
+      });
+    };
+    calculateTimes(0, maxT);
+    ++latticeIndex;
   });
-  */
+  console.log(edgesByTime);
+  model.edges = edges;
+  model.edgesByTime = edgesByTime;
+  model.maxT = maxT;
+  model.maxDt = maxDt;
+  // console.log([...phonemes].sort());
 })();
 
-const calculateTimes = (t, graph, index = 0) => {
-  const node = graph[index];
-  if (node.t != null) return;
-  if (node.in.length) {
-    node.t = t + node.in[0].dt;
-  } else {
-    node.t = 0;
-  }
-  node.out.forEach(({ i }) => calculateTimes(node.t, graph, i));
-};
+export const sortedPhonemes = [
+  "aa",
+  "ae",
+  "ah",
+  "ao",
+  "aw",
+  "ay",
+  "eh",
+  "er",
+  "ey",
+  "ih",
+  "iy",
+  "ow",
+  "oy",
+  "uh",
+  "uw",
+
+  "m",
+  "n",
+  "ng",
+
+  "ch",
+  "dh",
+  "jh",
+  "sh",
+  "th",
+  "zh",
+
+  "b",
+  "d",
+  "g",
+  "k",
+  "p",
+  "t",
+
+  "f",
+  "hh",
+  "s",
+  "v",
+  "w",
+  "z",
+
+  "l",
+  "r",
+  "y"
+
+  /*
+  "noise",
+  "oov",
+  "sil"
+  */
+];
