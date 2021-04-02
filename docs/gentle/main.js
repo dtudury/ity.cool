293756b780e84164aa36c9a3db07d12812c188b5
David Tudury <dt@reduct.video>
2021-04-02 12:18:42 -0700

    graph connections

--- docs/gentle/main.js
+++ docs/gentle/main.js
@@ -12,7 +12,10 @@ const phonemeToColor = phoneme => {
   }
 };
 const lines = el => {
-  const output = [];
+  const titleoutput = [];
+  const graphoutput = [];
+  const rectoutput = [];
+  const textoutput = [];
   const fileNames = Object.keys(model.files);
   const sectionHeight = model.innerHeight / fileNames.length;
   const offsetT = model.offsetT;
@@ -22,49 +25,75 @@ const lines = el => {
     const y = index * sectionHeight;
     const { maxDt, edgesByTime } = model.files[fileName];
     const tWidth = model.innerWidth / scale;
+    const toPoint = (i, t) => {
+      const _offsetT = t - offsetT;
+      const x = _offsetT * scale;
+      const y = -scrollY + height * 2 * (1 + i) + sectionHeight * index;
+      return { x, y };
+    };
     for (const t in edgesByTime) {
       if (t < offsetT + tWidth && t > offsetT - maxDt) {
-        const _offsetT = t - offsetT;
         const groups = edgesByTime[t];
         for (const groupString in groups) {
           const group = groups[groupString];
           const { transitions, word, dt } = JSON.parse(groupString);
-          const x = _offsetT * scale;
-          const y1 =
-            -scrollY + height * 2 * (1 + group.i) + sectionHeight * index;
-          if (y1 > y && y1 < y + sectionHeight) {
-            const y2 = y1 + height;
+          const p = toPoint(group.i, t);
+          if (p.y > y && p.y < y + sectionHeight) {
+            const onmouseover = el => e => {
+              model.selectedGroup = group;
+              model.selectedIndex = index;
+              el.onmouseout = () => {
+                model.selectedGroup = null;
+                model.selectedIndex = null;
+                el.onmouseout = null;
+              };
+            };
+            const y2 = p.y + height;
             const width = dt * scale;
-            output.push(h`
-              <rect x="${x}" y="${y1}" width="${width}" height="${height}" fill="white" stroke="rgb(155, 155, 155)"/>
+            rectoutput.push(h`
+              <rect 
+                onmouseover=${onmouseover}
+                x="${p.x}" 
+                y="${p.y}" 
+                width="${width}" 
+                height="${height}" 
+                fill="white" 
+                stroke="hsl(0, 0%, 90%)"
+              />
+            `);
+            textoutput.push(h`
               <text 
-                x="${x + width / 2}" 
-                y="${y1 + height / 2}" 
+                x="${p.x + width / 2}" 
+                y="${p.y + height / 2}" 
                 dominant-baseline="central"
                 text-anchor="middle"
+                style="pointer-events: none;"
               >${word}</text>
             `);
-            if (!isMoving) {
+            if (!isMoving && !model.selectedGroup) {
               let phoneX = 0;
               for (const { phone, length } of transitions) {
                 const shortPhone = phone.split("_")[0];
                 const color = phonemeToColor(shortPhone);
                 const width = length * scale;
-                output.push(h`
+                rectoutput.push(h`
                   <rect 
-                    x="${x + phoneX}" 
+                    x="${p.x + phoneX}" 
                     y="${y2}" 
                     width="${width}" 
                     height="${height}" 
                     fill="${color}" 
-                    stroke="rgb(155, 155, 155)"
+                    stroke="hsl(0, 0%, 90%)"
                   />
+                `);
+                textoutput.push(h`
                   <text 
                     fill="rgba(255, 255, 255, 50%)"
-                    x="${x + phoneX + width / 2}" 
+                    x="${p.x + phoneX + width / 2}" 
                     y="${y2 + height / 2}" 
                     dominant-baseline="central"
                     text-anchor="middle"
+                    style="pointer-events: none;"
                   >${shortPhone}</text>
                 `);
                 phoneX += width;
@@ -74,10 +103,52 @@ const lines = el => {
         }
       }
     }
-    output.push(h`
-      <rect x="0" y="${y}" width="${model.innerWidth}" height="${
-      height * 2
-    }" fill="hsl(0, 0%, 80%)" stroke="rgb(155, 155, 155)"/>
+    if (model.selectedGroup && model.selectedIndex === index) {
+      const drawEdge = (leftEdge, rightEdge) => {
+        const offsetI = 0.25;
+        const p0 = toPoint(
+          offsetI + leftEdge.i,
+          leftEdge.t + leftEdge.dt * 0.75
+        );
+        const p1 = toPoint(offsetI + leftEdge.i, leftEdge.t + leftEdge.dt);
+        const p2 = toPoint(offsetI + rightEdge.i, rightEdge.t);
+        const p3 = toPoint(
+          offsetI + rightEdge.i,
+          rightEdge.t + rightEdge.dt * 0.25
+        );
+        const space = p => `${p.x} ${p.y}`;
+        graphoutput.push(h`
+          <path 
+            d="
+              M ${space(p0)}
+              C ${space(p1)}
+                ${space(p2)}
+                ${space(p3)}
+            "
+            stroke="hsla(0, 0%, 70%, 50%)"
+            fill="none"
+            style="pointer-events: none;"
+          />
+        `);
+      };
+      for (const edge of model.selectedGroup.edges) {
+        for (const incomingEdge of edge.to) {
+          drawEdge(incomingEdge, model.selectedGroup);
+        }
+        for (const outgoingEdge of edge.from) {
+          drawEdge(model.selectedGroup, outgoingEdge);
+        }
+      }
+    }
+    titleoutput.push(h`
+      <rect 
+        x="0" 
+        y="${y}" 
+        width="${model.innerWidth}" 
+        height="${height * 2}" 
+        fill="hsl(0, 0%, 80%)" 
+        stroke="hsl(0, 0%, 70%)"
+      />
       <text 
         x="10" 
         y="${y + height}" 
@@ -87,7 +158,7 @@ const lines = el => {
       >${fileName}</text>
     `);
   });
-  return output;
+  return [rectoutput, graphoutput, textoutput, titleoutput];
 };
 
 let movingTimeout;
@@ -133,13 +204,13 @@ render(
       width: ${maxT}px;
       height: calc(100vh + ${maxH}px);
       overflow: scroll;
+      background: oldlace;
     }
     svg {
       position: fixed;
       height: 100vh;
       width: 100vw;
       display: block;
-      background: oldlace;
     }
   `
 );
--- docs/gentle/model.js
+++ docs/gentle/model.js
@@ -1,6 +1,10 @@
 import { proxy } from "./horseless.0.5.3.min.esm.js"; // '/unpkg/horseless/horseless.js'
 
-export const model = (window.model = proxy({ offsetT: 0, scrollY: 0, yOverflow: 0 }));
+export const model = (window.model = proxy({
+  offsetT: 0,
+  scrollY: 0,
+  yOverflow: 0
+}));
 
 const phonemes = new Set();
 const edgeToString = edge => {
@@ -39,6 +43,7 @@ const buildFromHash = async () => {
     const duration = edge =>
       (edge.transitions || []).reduce((a, { length }) => a + length, 0);
     const edgesByFrom = {};
+    const edgesByTo = {};
     for (const i in lattice) {
       for (const edge of lattice[i]) {
         const edgeIndex = edges.length;
@@ -49,19 +54,25 @@ const buildFromHash = async () => {
         const fromState = edge.from_state;
         if (toState != null && fromState != null) {
           edgesByFrom[fromState] = edgesByFrom[fromState] || [];
-          edgesByFrom[fromState].push({ toState, edgeIndex });
+          edgesByFrom[fromState].push({ fromState, toState, edgeIndex });
+          edgesByTo[toState] = edgesByTo[toState] || [];
+          edgesByTo[toState].push({ fromState, toState, edgeIndex });
         }
       }
     }
+    const groupByEdge = {};
     const calculateTimes = (state, t) => {
       (edgesByFrom[state] || []).forEach(({ toState, edgeIndex }) => {
         edgesByTime[t] = edgesByTime[t] || {};
         const str = edgeToString(edges[edgeIndex]);
+        edges[edgeIndex].t = t;
         const dt = edges[edgeIndex].dt;
         const group = (edgesByTime[t][str] = edgesByTime[t][str] || {
           edges: [],
-          dt
+          dt,
+          t
         });
+        groupByEdge[edgeIndex] = group;
         maxT = Math.max(maxT, t + dt);
         if (!group.edges.includes(edgeIndex)) {
           group.edges.push(edgeIndex);
@@ -84,7 +95,33 @@ const buildFromHash = async () => {
         positions[i] = +time + group.dt;
       }
     }
-    model.files[jsonUrl] = { edges, edgesByTime, maxT, maxDt, maxStack: positions.length };
+    for (const time in edgesByTime) {
+      for (const str in edgesByTime[time]) {
+        const group = edgesByTime[time][str];
+        group.edges = group.edges.map(edgeIndex => {
+          const edge = edges[edgeIndex];
+          const { to_state, from_state } = edge;
+          const mapEdges = edge => {
+            const { dt, t, i } = groupByEdge[edge.edgeIndex];
+            return { dt, t, i, edgeIndex: edge.edgeIndex };
+          };
+          return {
+            to: (edgesByTo[from_state] || []).map(mapEdges),
+            from: (edgesByFrom[to_state] || []).map(mapEdges),
+            edge,
+            edgeIndex
+          };
+        });
+      }
+    }
+
+    model.files[jsonUrl] = {
+      edges,
+      edgesByTime,
+      maxT,
+      maxDt,
+      maxStack: positions.length
+    };
   }
   // console.log([...phonemes].sort());
 };
@@ -136,7 +173,7 @@ export const sortedPhonemes = [
   "s",
   "v",
   "w",
-  "z",
+  "z"
 
   /*
   "noise",
